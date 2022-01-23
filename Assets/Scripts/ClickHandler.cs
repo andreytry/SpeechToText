fbf05f183ade83f306442a6a07ccaee9a97314e7
andreytry <andreytry@gmail.com>
2022-01-23 12:21:03 +0100

    first commit

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour
+{
+    public UnityEvent downEvent;
+    public UnityEvent upEvent;
+
+    Color originalColor;
+    Renderer componentRenderer;
+
+    private void Start()
+    {
+        componentRenderer = GetComponent<Renderer>();
+        originalColor = componentRenderer.material.color;
+    }
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("Down");
+        downEvent?.Invoke();
+        Highlight(true);
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Up");
+        upEvent?.Invoke();
+        Highlight(false);
+    }
+
+    void Highlight(bool isHighlighted)
+    {
+        if (isHighlighted)
+        {
+            componentRenderer.material.SetColor("_Color", Color.blue);
+        } else
+        {
+            componentRenderer.material.SetColor("_Color", originalColor);
+        }
+    }
+}
---
+++ Assets/Scripts/VoiceController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextSpeech;
+using UnityEngine.UI;
+using UnityEngine.Android;
+
+
+public class VoiceController : MonoBehaviour
+{
+    const string LANG_CODE = "en-US";
+
+    [SerializeField]
+    Text uiText;
+
+    private void Start()
+    {
+        Setup(LANG_CODE);
+
+        SpeechToText.instance.onResultCallback = OnFinalSpeechResult;
+#if UNITY_ANDROID
+        SpeechToText.instance.onPartialResultsCallback = OnPartialSpeechResult;
+#endif
+
+        TextToSpeech.instance.onStartCallBack = OnSpeakStart;
+        TextToSpeech.instance.onDoneCallback = OnSpeakStop;
+        CheckPermission();
+    }
+    void Setup(string code)
+    {
+        TextToSpeech.instance.Setting(code, 1, 1);
+        SpeechToText.instance.Setting(code);
+    }
+
+    void CheckPermission()
+    {
+#if UNITY_ANDROID
+        if (! Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+#endif
+    }
+
+#region Text to speech
+    public void StartSpeaking(string message)
+    {
+        TextToSpeech.instance.StartSpeak(message);
+    }
+
+    void OnSpeakStart()
+    {
+        Debug.Log("Started speaking...");
+    }
+
+    void OnSpeakStop()
+    {
+        Debug.Log("Stopped speaking...");
+    }
+#endregion
+
+#region Speech to text
+
+    public void StartListening()
+    {
+        SpeechToText.instance.StartRecording();
+        Debug.Log("StartListning");
+    }
+    public void StopListening()
+    {
+        SpeechToText.instance.StopRecording();
+        Debug.Log("StopListning");
+    }
+
+    void OnFinalSpeechResult(string result)
+    {
+        uiText.text = result;
+    }
+
+    void OnPartialSpeechResult(string result)
+    {
+        uiText.text = result;
+    }
+
+    public void StopSpealing()
+    {
+        TextToSpeech.instance.StopSpeak();
+    }
+
+    #endregion
+
+
+}
